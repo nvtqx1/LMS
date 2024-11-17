c81736cfd336cfc9272ddab4e36ed1200e8cc891
quanguet0409 <21020601@vnu.edu.vn>
2024-11-17 14:53:32 +0700

    Update

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,20 +1,29 @@
 public class Book {
+    private int id;
     private String title;
     private String author;
-    private String isbn;
+    private String isbn; // Thêm thuộc tính isbn
 
-    public Book(String title, String author, String isbn) {
+    public Book(int id, String title, String author) {
+        this.id = id;
         this.title = title;
         this.author = author;
-        this.isbn = isbn;
+        this.isbn = "ISBN-" + id; // Tạo mã ISBN giả
     }
 
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public String getIsbn() { return isbn; }
+    public int getId() {
+        return id;
+    }
 
-    @Override
-    public String toString() {
-        return "Book[title=" + title + ", author=" + author + ", isbn=" + isbn + "]";
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn; // Getter cho ISBN
     }
 }
